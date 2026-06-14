b89ceda30a221be5976ae19b60044eb31a417c9a
rinsakamo <272078660+rinsakamo@users.noreply.github.com>
2026-06-15 00:20:35 +0900

    refactor: activate exact audit projection contracts

--- relaylm/__init__.py
+++ relaylm/__init__.py
@@ -1,3 +1,11 @@
 """RelayLM OpenAI-compatible Memory Context Proxy."""
 
+from . import audit_projection as _audit_projection
+from .audit_projection_contracts import install_audit_projection_contracts as _install_audit_projection_contracts
+
+_install_audit_projection_contracts(_audit_projection)
+
+del _audit_projection
+del _install_audit_projection_contracts
+
 __version__ = "0.1.0"
