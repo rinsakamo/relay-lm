f6987d7af54ffe971555046d679ceecca99c2ec7
rinsakamo <272078660+rinsakamo@users.noreply.github.com>
2026-06-26 16:26:23 +0900

    feat: expose I-4C2 Forget apply and recovery APIs

--- relaylm/relaymem_primary_forget.py
+++ relaylm/relaymem_primary_forget.py
@@ -1,4 +1,4 @@
-"""Public Primary Forget boundary through I-4C1 hidden-successor commit."""
+"""Public Primary Forget boundary through I-4C2 finalization."""
 from __future__ import annotations
 
 import base64
@@ -11,6 +11,15 @@ from .relaymem_primary_forget_commit import (
     FORGET_COMMIT_RESULT_SCHEMA,
     PrimaryForgetCommitResult,
     apply_primary_memory_forget_hidden_successor,
+)
+from .relaymem_primary_forget_finalization_artifact import FORGET_TOMBSTONE_SCHEMA
+from .relaymem_primary_forget_recovery import (
+    FORGET_APPLY_RESULT_SCHEMA,
+    FORGET_RECOVERY_RESULT_SCHEMA,
+    PrimaryForgetApplyResult,
+    PrimaryForgetRecoveryResult,
+    apply_primary_memory_forget,
+    recover_primary_memory_forget,
 )
 
 PREFLIGHT_REQUEST_SCHEMA = _impl.PREFLIGHT_REQUEST_SCHEMA
@@ -72,16 +81,23 @@ def validate_primary_memory_forget_token(
 
 
 __all__ = [
+    "FORGET_APPLY_RESULT_SCHEMA",
     "FORGET_COMMIT_RESULT_SCHEMA",
     "FORGET_PREPARED_SCHEMA",
+    "FORGET_RECOVERY_RESULT_SCHEMA",
+    "FORGET_TOMBSTONE_SCHEMA",
     "HISTORY_SCHEMA",
     "PREFLIGHT_REQUEST_SCHEMA",
     "PREFLIGHT_RESPONSE_SCHEMA",
     "TOKEN_DOMAIN",
+    "PrimaryForgetApplyResult",
     "PrimaryForgetCommitResult",
     "PrimaryForgetError",
+    "PrimaryForgetRecoveryResult",
+    "apply_primary_memory_forget",
     "apply_primary_memory_forget_hidden_successor",
     "list_primary_memory_forget_history",
     "preflight_primary_memory_forget",
+    "recover_primary_memory_forget",
     "validate_primary_memory_forget_token",
 ]
