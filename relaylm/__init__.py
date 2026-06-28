bf97d3c28997be9833ca881f4e1821b28f52ae50
rinsakamo <272078660+rinsakamo@users.noreply.github.com>
2026-06-28 19:11:51 +0900

    Install scoped Primary recall runtime hooks

--- relaylm/__init__.py
+++ relaylm/__init__.py
@@ -4,6 +4,9 @@ from . import audit_projection as _audit_projection
 from .audit_projection_contracts import (
     install_audit_projection_contracts as _install_audit_projection_contracts,
 )
+from .relaymem_primary_recall_runtime import (
+    install_relaymem_primary_recall_runtime as _install_relaymem_primary_recall_runtime,
+)
 from .relaymem_retrieval_priority_runtime import (
     install_relaymem_retrieval_priority_runtime as _install_relaymem_retrieval_priority_runtime,
 )
@@ -16,9 +19,11 @@ _audit_projection.TOP_LEVEL_PROJECTORS[
     "projection_unsupported_artifact_count"
 ] = _audit_projection._non_negative_int
 _install_relaymem_retrieval_priority_runtime()
+_install_relaymem_primary_recall_runtime()
 
 del _audit_projection
 del _install_audit_projection_contracts
+del _install_relaymem_primary_recall_runtime
 del _install_relaymem_retrieval_priority_runtime
 
 __version__ = "0.1.0"
