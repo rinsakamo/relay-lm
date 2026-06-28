44ec7f3d2a8cc627a5dd6af00d6ae4709b7f2691
rinsakamo <272078660+rinsakamo@users.noreply.github.com>
2026-06-28 21:04:29 +0900

    fix: align E1-R5 runtime installers with scoped primary recall

--- relaylm/__init__.py
+++ relaylm/__init__.py
@@ -7,6 +7,9 @@ from .audit_projection_contracts import (
 from .relaymem_primary_recall_candidate_bridge_runtime import (
     install_relaymem_primary_recall_candidate_bridge_runtime as _install_relaymem_primary_recall_candidate_bridge_runtime,
 )
+from .relaymem_primary_recall_runtime import (
+    install_relaymem_primary_recall_runtime as _install_relaymem_primary_recall_runtime,
+)
 from .relaymem_retrieval_priority_runtime import (
     install_relaymem_retrieval_priority_runtime as _install_relaymem_retrieval_priority_runtime,
 )
@@ -19,11 +22,13 @@ _audit_projection.TOP_LEVEL_PROJECTORS[
     "projection_unsupported_artifact_count"
 ] = _audit_projection._non_negative_int
 _install_relaymem_retrieval_priority_runtime()
+_install_relaymem_primary_recall_runtime()
 _install_relaymem_primary_recall_candidate_bridge_runtime()
 
 del _audit_projection
 del _install_audit_projection_contracts
-del _install_relaymem_retrieval_priority_runtime
 del _install_relaymem_primary_recall_candidate_bridge_runtime
+del _install_relaymem_primary_recall_runtime
+del _install_relaymem_retrieval_priority_runtime
 
 __version__ = "0.1.0"
