2066c0758c160d52fac7d08ee0b2828e813d2ed0
rinsakamo <272078660+rinsakamo@users.noreply.github.com>
2026-09-10 22:30:27 +0900

    feat(theory): include finite monic splitting in root

--- formal/relay_theory/RelayTheory.lean
+++ formal/relay_theory/RelayTheory.lean
@@ -34,4 +34,5 @@ import RelayTheory.DiracRecoverySharpness
 import RelayTheory.SequentialRecoveryReflection
 import RelayTheory.RecoveryCancellation
 import RelayTheory.FinitePivot
+import RelayTheory.FiniteMonicSplitting
 import RelayTheory.Countermodels
