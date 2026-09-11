cf02780c95d5362919678e4ce2ec05c70594c6e2
rinsakamo <272078660+rinsakamo@users.noreply.github.com>
2026-09-12 05:54:36 +0900

    feat(theory): expose representation-family expansion module (#2629)

--- formal/relay_theory/RelayTheory.lean
+++ formal/relay_theory/RelayTheory.lean
@@ -16,6 +16,7 @@ import RelayTheory.BehavioralQuotient
 import RelayTheory.RepresentationFactorization
 import RelayTheory.RepresentationFamily
 import RelayTheory.RepresentationRefinement
+import RelayTheory.RepresentationFamilyExpansion
 import RelayTheory.ProbeRelative
 import RelayTheory.ObservationalPredictiveGap
 import RelayTheory.ProbeDoctrine
