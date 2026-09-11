a8fa8e0649c184b5ac2685e76e4334daf3a68225
rinsakamo <272078660+rinsakamo@users.noreply.github.com>
2026-09-11 11:27:47 +0900

    feat(theory): export hidden-carrier return theorem (#2557)

--- formal/relay_theory/RelayTheory.lean
+++ formal/relay_theory/RelayTheory.lean
@@ -44,4 +44,5 @@ import RelayTheory.CausalOrderProjection
 import RelayTheory.ObserverChainRealizers
 import RelayTheory.ProcessMonotonicity
 import RelayTheory.IntrinsicKernelDegradation
+import RelayTheory.HiddenCarrierReturn
 import RelayTheory.Countermodels
